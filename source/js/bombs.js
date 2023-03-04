f5dbe4e35e14c5141bb9a81eda973e2ebe67e7d4
lilgikki <katy.kostyleva2010@gmail.com>
2023-03-04 16:43:12 +0300

    added showing cells after click empty cell

---
+++ source/js/bombs.js
@@ -0,0 +1,67 @@
+const bombsDiv = document.querySelector('.bombs');
+const bombs = bombsDiv.querySelectorAll('div');
+
+const setCount = (block, k) => {
+  if (k == 1) {
+    block.classList.remove('bombs__0');
+    block.classList.remove('bombs__1');
+    block.classList.remove('bombs__2');
+    block.classList.add('bombs__1');
+  } else if (k == 2) {
+    block.classList.remove('bombs__1');
+    block.classList.remove('bombs__2');
+    block.classList.remove('bombs__3');
+    block.classList.add('bombs__2');
+  } else if (k == 3) {
+    block.classList.remove('bombs__2');
+    block.classList.remove('bombs__3');
+    block.classList.remove('bombs__4');
+    block.classList.add('bombs__3');
+  } else if (k == 4) {
+    block.classList.remove('bombs__3');
+    block.classList.remove('bombs__4');
+    block.classList.remove('bombs__5');
+    block.classList.add('bombs__4');
+  } else if (k == 5) {
+    block.classList.remove('bombs__4');
+    block.classList.remove('bombs__5');
+    block.classList.remove('bombs__6');
+    block.classList.add('bombs__5');
+  } else if (k == 6) {
+    block.classList.remove('bombs__5');
+    block.classList.remove('bombs__6');
+    block.classList.remove('bombs__7');
+    block.classList.add('bombs__6');
+  } else if (k == 7) {
+    block.classList.remove('bombs__6');
+    block.classList.remove('bombs__7');
+    block.classList.remove('bombs__8');
+    block.classList.add('bombs__7');
+  } else if (k == 8) {
+    block.classList.remove('bombs__7');
+    block.classList.remove('bombs__8');
+    block.classList.remove('bombs__9');
+    block.classList.add('bombs__8');
+  } else if (k == 9) {
+    block.classList.remove('bombs__8');
+    block.classList.remove('bombs__9');
+    block.classList.remove('bombs__0');
+    block.classList.add('bombs__9');
+  } else if (k == 0) {
+    block.classList.remove('bombs__9');
+    block.classList.remove('bombs__0');
+    block.classList.remove('bombs__1');
+    block.classList.add('bombs__0');
+  }
+};
+
+const bombsCounter = (k) => {
+  const k1 = k % 10;
+  const k2 = Math.floor((k % 100 - k1) / 10);
+  const k3 = Math.floor((k % 1000 - k2*10 - k1) / 100);
+  setCount(bombs[0], k3);
+  setCount(bombs[1], k2);
+  setCount(bombs[2], k1);
+};
+
+export {bombsCounter};
--- source/js/sapper.js
+++ source/js/sapper.js
@@ -1,4 +1,5 @@
 import {changeTime} from './time.js';
+import {bombsCounter} from './bombs.js';
 
 const FIELD__SIZE = 16;
 const BOMBS_COUNT = 40;
@@ -19,7 +20,11 @@ const generateArrayRandomNumber = (min, max, firstNum) => {
 
   while (arrayRandomNumbers.length < BOMBS_COUNT) {
     tempRandomNumber = Math.round(Math.random() * (arrayTotalNumbers.length - 1));
-    if (arrayTotalNumbers[tempRandomNumber] !== firstNum) {
+
+    // Условие того, бомба не поставится на первую нажаю ячейку или на ее соседей
+    const emptyFirstClick = (arrayTotalNumbers[tempRandomNumber] !== firstNum) && (arrayTotalNumbers[tempRandomNumber] !== (firstNum - 1)) && (arrayTotalNumbers[tempRandomNumber] !== (firstNum + 1)) && (arrayTotalNumbers[tempRandomNumber] !== (firstNum - 1 + FIELD__SIZE)) && (arrayTotalNumbers[tempRandomNumber] !== (firstNum + 1 + FIELD__SIZE)) && (arrayTotalNumbers[tempRandomNumber] !== (firstNum + FIELD__SIZE)) && (arrayTotalNumbers[tempRandomNumber] !== (firstNum - 1 - FIELD__SIZE)) && (arrayTotalNumbers[tempRandomNumber] !== (firstNum + 1 - FIELD__SIZE)) && (arrayTotalNumbers[tempRandomNumber] !== (firstNum - FIELD__SIZE));
+    // Бомба не попадает на первую ячейке или на ее соседей, то она записывается в массив "бомб"
+    if (emptyFirstClick) {
       arrayRandomNumbers.push(arrayTotalNumbers[tempRandomNumber]);
       arrayTotalNumbers.splice(tempRandomNumber, 1);
     }
@@ -72,22 +77,110 @@ const renderArray = (l) => {
 };
 
 const activateCell = () => {
+  let countBombs = BOMBS_COUNT;
   let arr;
+  let arrChecked = [];
   const cells = field.querySelectorAll('.field__cell');
 
   cells.forEach((cell, i) => {
-    cell.addEventListener('mousedown', () => {
-      // Испуганный смайлик
-      if (playButton.classList.contains('sapper__play--start')) {
+    // Испуганный смайлик
+    const scaryEmoji = () => {
+      if (playButton.classList.contains('sapper__play--start') && cell.classList.contains('field__cell--closed')) {
         playButton.classList.remove('sapper__play--start');
         playButton.classList.add('sapper__play--scary');
       }
-    });
+    };
 
-    cell.addEventListener('mouseup', () => {
+    // Клик на ячейку
+    const clickCell = () => {
+
+      // Открытие ячейки, в соотсветсвие с ее значeнием в массиве и ее соседями
+      const showCell = (a, x, y) => {
+        switch(a[x][y]) {
+          case 0:
+            cells[x*FIELD__SIZE + y].classList.add('field__cell--opened');
+            break;
+          case 1:
+            cells[x*FIELD__SIZE + y].classList.add('field__cell--1');
+            break;
+          case 2:
+            cells[x*FIELD__SIZE + y].classList.add('field__cell--2');
+            break;
+          case 3:
+            cells[x*FIELD__SIZE + y].classList.add('field__cell--3');
+            break;
+          case 4:
+            cells[x*FIELD__SIZE + y].classList.add('field__cell--4');
+            break;
+          case 5:
+            cells[x*FIELD__SIZE + y].classList.add('field__cell--5');
+            break;
+          case 6:
+            cells[x*FIELD__SIZE + y].classList.add('field__cell--6');
+              break;
+          case 7:
+            cells[x*FIELD__SIZE + y].classList.add('field__cell--7');
+              break;
+          case 8:
+            cells[x*FIELD__SIZE + y].classList.add('field__cell--8');
+              break;
+          case 9:
+            cells[x*FIELD__SIZE + y].classList.add('field__cell--bomb-act');
+              break;
+        }
+        return;
+      };
+
+      // Открытие соседних пустых ячеек
+      const showEmptyNeighbours = (a, x, y) => {
+        if (x > 0 && arrChecked[x-1][y] == 0 && (cells[(x-1)*FIELD__SIZE + y].classList.contains('field__cell--closed'))) {
+          cells[(x-1)*FIELD__SIZE + y].classList.remove('field__cell--closed');
+          cells[(x-1)*FIELD__SIZE + y].classList.remove('field__cell--quest');
+          showCell(a, x-1, y);
+          arrChecked[x-1][y] = 1;
+          if (a[x-1][y] == 0) {
+            showEmptyNeighbours(a, x-1, y);
+          }
+        }
+        if (y > 0 && arrChecked[x][y-1] == 0 && (cells[x*FIELD__SIZE + (y-1)].classList.contains('field__cell--closed'))) {
+          cells[x*FIELD__SIZE + (y-1)].classList.remove('field__cell--closed');
+          cells[x*FIELD__SIZE + (y-1)].classList.remove('field__cell--quest');
+          showCell(a, x, y-1);
+          arrChecked[x][y-1] = 1;
+          if (a[x][y-1] == 0) {
+            showEmptyNeighbours(a, x, y-1);
+          }
+        }
+        if (y < FIELD__SIZE - 1 && arrChecked[x][y+1] == 0 && (cells[x*FIELD__SIZE + (y+1)].classList.contains('field__cell--closed'))) {
+          cells[x*FIELD__SIZE + (y+1)].classList.remove('field__cell--closed');
+          cells[x*FIELD__SIZE + (y+1)].classList.remove('field__cell--quest');
+          showCell(a, x, y+1);
+          arrChecked[x][y+1] = 1;
+          if (a[x][y+1] == 0) {
+            showEmptyNeighbours(a, x, y+1);
+          }
+        }
+        if (x < FIELD__SIZE - 1 && arrChecked[x+1][y] == 0 && (cells[(x+1)*FIELD__SIZE + y].classList.contains('field__cell--closed'))) {
+          cells[(x+1)*FIELD__SIZE + y].classList.remove('field__cell--closed');
+          cells[(x+1)*FIELD__SIZE + y].classList.remove('field__cell--quest');
+          showCell(a, x+1, y);
+          arrChecked[x+1][y] = 1;
+          if (a[x+1][y] == 0) {
+            showEmptyNeighbours(a, x+1, y);
+          }
+        }
+        return;
+      };
+
       // Первый клик или нет
       if (firstClick) {
         arr = renderArray(i);
+        for (let o = 0; o < FIELD__SIZE; o++) {
+          arrChecked[o] = [];
+          for (let p = 0; p < FIELD__SIZE; p++) {
+            arrChecked[o][p] = 0;
+          }
+        }
         firstClick = false;
         gameActive = true;
         changeTime();
@@ -99,43 +192,53 @@ const activateCell = () => {
         playButton.classList.remove('sapper__play--scary');
       }
 
+      // Открытие ячейки
       if (cell.classList.contains('field__cell--closed')) {
         const n = Math.floor(i / FIELD__SIZE);
         const m = i % FIELD__SIZE;
         cell.classList.remove('field__cell--closed');
-
-        switch(arr[n][m]) {
-          case 0:
-            cell.classList.add('field__cell--opened');
-            break;
-          case 1:
-            cell.classList.add('field__cell--1');
-            break;
-          case 2:
-            cell.classList.add('field__cell--2');
-            break;
-          case 3:
-            cell.classList.add('field__cell--3');
-            break;
-          case 4:
-            cell.classList.add('field__cell--4');
-            break;
-          case 5:
-            cell.classList.add('field__cell--5');
-            break;
-          case 6:
-              cell.classList.add('field__cell--6');
-              break;
-          case 7:
-              cell.classList.add('field__cell--7');
-              break;
-          case 8:
-              cell.classList.add('field__cell--8');
-              break;
-          case 9:
-              cell.classList.add('field__cell--bomb-act');
-              break;
+        showCell(arr, n, m);
+        arrChecked[n][m] = 1;
+        if (arr[n][m] == 0) {
+          showEmptyNeighbours(arr, n, m);
         }
+      }
+    };
+
+    const getFlag = () => {
+      if (cell.classList.contains('field__cell--closed')) {
+        cell.classList.remove('field__cell--closed');
+        cell.classList.add('field__cell--flag');
+        countBombs--;
+        bombsCounter(countBombs);
+      } else if (cell.classList.contains('field__cell--flag')) {
+        cell.classList.remove('field__cell--flag');
+        cell.classList.add('field__cell--quest');
+        countBombs++;
+        bombsCounter(countBombs);
+      } else if (cell.classList.contains('field__cell--quest')) {
+        cell.classList.remove('field__cell--quest');
+        cell.classList.add('field__cell--closed');
+      }
+    }
+
+    cell.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+    })
+
+    cell.addEventListener('mouseup', (e) => {
+      e.preventDefault();
+      if (e.button == 2) {
+        getFlag();
+      } else if (e.button == 0) {
+        clickCell();
+      }
+    })
+
+    cell.addEventListener('mousedown', (e) => {
+      e.preventDefault();
+      if (e.button == 0) {
+        scaryEmoji();
       }
     });
   })
